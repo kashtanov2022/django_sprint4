35d1f9f7942727d3cab9f0a03981b778821a34d0
Invisi <189375571+kashtanov2022@users.noreply.github.com>
2025-03-22 14:24:08 +0700

    Post editing is updated

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -63,7 +63,7 @@ def edit_post(request, post_id):
     post = get_object_or_404(Post, id=post_id)
     if request.user != post.author:
         return redirect('blog:post_detail', post_id)
-    form = PostForm(request.POST or None, instance=post)
+    form = PostForm(request.POST or None, files=request.FILES or None, instance=post)
     if form.is_valid():
         form.save()
         return redirect('blog:post_detail', post_id)
